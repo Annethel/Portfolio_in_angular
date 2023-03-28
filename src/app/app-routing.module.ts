fc62f0a589e2c5a17a92a6e232fc3b0fd72c2829
Anna Thelma <annathelma20@gmail.com>
2023-03-28 06:54:45 +0100

    fixed the route issue and made it scrollable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { ServicesComponent } from './services/services.component';
 import { ContainerComponent } from './container/container.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
-  { path: ' ', component: ContainerComponent },
+  { path: 'Home', component: HomepageComponent },
   { path: 'About', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'service', component: ServicesComponent }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+
+  constructor(private activatedRoute: ActivatedRoute){}
+  ngOnInit(){
+     this.activatedRoute.fragment.subscribe((val) => {
+      console.log(val);
+      this.jumpTo(val ??" ");
+     });
+   }
+
+   jumpTo(section:string){
+    document.getElementById(section)?.scrollIntoView({behavior: 'smooth'});
+   }
 }
